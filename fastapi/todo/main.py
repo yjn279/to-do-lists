1f155bcdcd4418e982cd86c364f69fed48c51624
Nakamura Yuji <57951904+Yuji-207@users.noreply.github.com>
2022-12-29 23:37:16 +0900

    feat: main.pyに更新・削除のエンドポイントを追加

--- fastapi/todo/main.py
+++ fastapi/todo/main.py
@@ -1,6 +1,6 @@
 from sqlalchemy.orm import Session
 
-from fastapi import Depends, FastAPI, HTTPException
+from fastapi import Depends, FastAPI
 
 from . import crud, models, schemas
 from .database import SessionLocal, engine
@@ -19,38 +19,69 @@ def get_db():
         db.close()
 
 
-@app.post("/users/", response_model=schemas.User)
-def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    db_user = crud.get_user_by_email(db, email=user.email)
-    if db_user:
-        raise HTTPException(status_code=400, detail="Email already registered")
-    return crud.create_user(db=db, user=user)
-
-
 @app.get("/users/", response_model=list[schemas.User])
 def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    users = crud.get_users(db, skip=skip, limit=limit)
-    return users
+    return crud.get_users(db, skip=skip, limit=limit)
+
+
+@app.post("/users/", response_model=schemas.User)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    return crud.create_user(db=db, user=user)
 
 
 @app.get("/users/{user_id}", response_model=schemas.User)
 def read_user(user_id: int, db: Session = Depends(get_db)):
-    db_user = crud.get_user(db, user_id=user_id)
-    if db_user is None:
-        raise HTTPException(status_code=404, detail="User not found")
-    return db_user
+    return crud.get_user(db, user_id=user_id)
+
+
+@app.put("/users/{user_id}", response_model=schemas.User)
+def update_user(
+    user_id,
+    user: schemas.UserCreate,
+    db: Session = Depends(get_db),
+):
+    return crud.update_user(db, user_id=user_id, user=user)
+
+
+@app.delete("/users/{user_id}", response_model=schemas.User)
+def delete_user(user_id: int, db: Session = Depends(get_db)):
+    return crud.delete_user(db, user_id=user_id)
+
+
+@app.get("/users/{user_id}/tasks/", response_model=list[schemas.Task])
+def read_tasks(
+    user_id: int,
+    skip: int = 0,
+    limit: int = 100,
+    db: Session = Depends(get_db),
+):
+    return crud.get_tasks(db, owner_id=user_id, skip=skip, limit=limit)
 
 
 @app.post("/users/{user_id}/tasks/", response_model=schemas.Task)
-def create_task_for_user(
+def create_task(
     user_id: int,
     task: schemas.TaskCreate,
     db: Session = Depends(get_db),
 ):
-    return crud.create_user_task(db=db, task=task, user_id=user_id)
+    return crud.create_task(db=db, task=task, user_id=user_id)
 
 
-@app.get("/tasks/", response_model=list[schemas.Task])
-def read_tasks(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    tasks = crud.get_tasks(db, skip=skip, limit=limit)
-    return tasks
+@app.get("/users/{user_id}/tasks/{task_id}", response_model=schemas.Task)
+def read_task(user_id: int, task_id: int, db: Session = Depends(get_db)):
+    return crud.get_task(db, task_id=task_id)
+
+
+@app.put("/users/{user_id}/tasks/{task_id}", response_model=schemas.Task)
+def update_task(
+    user_id: int,
+    task_id: int,
+    task: schemas.TaskCreate,
+    db: Session = Depends(get_db),
+):
+    return crud.update_task(db, task_id=task_id, task=task, user_id=user_id)
+
+
+@app.delete("/users/{user_id}/tasks/{task_id}", response_model=schemas.Task)
+def delete_task(user_id: int, task_id: int, db: Session = Depends(get_db)):
+    return crud.delete_task(db, task_id=task_id)
