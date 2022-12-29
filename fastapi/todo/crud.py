ff8a81bbee0494ff77cfedd98c180f04cd84ae26
Nakamura Yuji <57951904+Yuji-207@users.noreply.github.com>
2022-12-29 22:57:47 +0900

    feat: crud.pyに更新・削除を実装

--- fastapi/todo/crud.py
+++ fastapi/todo/crud.py
@@ -5,21 +5,21 @@ from sqlalchemy.orm import Session
 from . import models, schemas
 
 
-def get_user(db: Session, user_id: int):
+def get_users(
+    db: Session,
+    skip: int = 0,
+    limit: int = 100,
+) -> list[models.User]:
+    return db.query(models.Use).offset(skip).limit(limit).all()
+
+
+def get_user(db: Session, user_id: int) -> models.User:
     return db.query(models.User).filter(models.User.id == user_id).first()
 
 
-def get_user_by_email(db: Session, email: str):
-    return db.query(models.User).filter(models.User.email == email).first()
-
-
-def get_users(db: Session, skip: int = 0, limit: int = 100):
-    return db.query(models.User).offset(skip).limit(limit).all()
-
-
-def create_user(db: Session, user: schemas.UserCreate):
+def create_user(db: Session, user: schemas.UserCreate) -> models.User:
     db_user = models.User(
-        email=user.email,
+        **user.dict(),
         password=hashlib.sha512(user.password).hexdigest(),
     )
     db.add(db_user)
@@ -28,13 +28,56 @@ def create_user(db: Session, user: schemas.UserCreate):
     return db_user
 
 
-def get_tasks(db: Session, skip: int = 0, limit: int = 100):
+def update_user(db: Session, user: schemas.UserUpdate) -> models.User:
+    db_user = get_user(db=db, user_id=user.id)
+    db_user.name = user.name
+    db_user.email = user.email
+    db_user.password = user.hashlib.sha512(user.password).hexdigest()
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+
+def delete_user(db: Session, user_id: int) -> models.User:
+    db_user = get_user(db=db, user_id=user_id)
+    db.delete(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+
+def get_tasks(
+    db: Session,
+    skip: int = 0,
+    limit: int = 100,
+) -> list[models.Task]:
     return db.query(models.Task).offset(skip).limit(limit).all()
 
 
-def create_user_task(db: Session, task: schemas.TaskCreate, user_id: int):
+def get_task(db: Session, task_id: int) -> models.Task:
+    return db.query(models.Task).filter(models.Task.id == task_id).first()
+
+
+def create_task(db: Session, task: schemas.TaskCreate, user_id: int) -> models.Task:
     db_task = models.Task(**task.dict(), owner_id=user_id)
     db.add(db_task)
+    db.commit()
+    db.refresh(db_task)
+    return db_task
+
+
+def update_task(db: Session, task: schemas.taskUpdate, user_id: int) -> models.Task:
+    db_task = get_task(db=db, task_id=task.id)
+    db_task.title = task.title
+    db_task.owner_id = user_id
+    db.commit()
+    db.refresh(db_task)
+    return db_task
+
+
+def delete_task(db: Session, task_id: int) -> models.Task:
+    db_task = get_task(db=db, task_id=task_id)
+    db.delete(db_task)
     db.commit()
     db.refresh(db_task)
     return db_task
