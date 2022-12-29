c085e00b092524a55daaaef9650919f7906c4c81
Nakamura Yuji <57951904+Yuji-207@users.noreply.github.com>
2022-12-29 22:22:12 +0900

    feat: schemas.pyに更新・削除用のスキーマを追加

--- fastapi/todo/schemas.py
+++ fastapi/todo/schemas.py
@@ -1,33 +1,52 @@
 from pydantic import BaseModel
 
 
-class TaskBase(BaseModel):
-    title: str
-
-
-class TaskCreate(TaskBase):
-    pass
-
-
-class Task(TaskBase):
+class TaskGet(BaseModel):
     id: int
+    title: str
     owner_id: int
 
     class Config:
         orm_mode = True
 
 
-class UserBase(BaseModel):
-    email: str
+class TaskCreate(BaseModel):
+    title: str
+    owner_id: int
 
 
-class UserCreate(UserBase):
-    password: str
-
-
-class User(UserBase):
+class TaskUpdate(BaseModel):
     id: int
-    tasks: list[Task] = []
+    title: str
+    owner_id: int
+
+
+class TaskDelete(BaseModel):
+    id: int
+
+
+class UserGet(BaseModel):
+    id: int
+    name: str
+    email: str
+    tasks: list[TaskGet] = []
 
     class Config:
         orm_mode = True
+
+
+class UserCreate(BaseModel):
+    name: str
+    email: str
+    password: str
+
+
+class UserUpdate(BaseModel):
+    id: int
+    name: str
+    email: str
+    password: str
+
+
+class UserDelete(BaseModel):
+    id: int
